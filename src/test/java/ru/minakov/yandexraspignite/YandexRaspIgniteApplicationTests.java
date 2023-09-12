1275700afd98229a283662f676eaa6fbf25fb615
yminakov <y.minakov@pulkovo-airport.com>
2023-09-12 10:39:21 +0300

    Init commit

---
+++ src/test/java/ru/minakov/yandexraspignite/YandexRaspIgniteApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.minakov.yandexraspignite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YandexRaspIgniteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
